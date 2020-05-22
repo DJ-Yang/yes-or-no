28b636a0a299e4584452fab6d2c56cb0f91f93dc
unknown <parksanghyup>
2020-05-22 17:03:33 +0900

    django-allauth init

--- yorn/settings/base.py
+++ yorn/settings/base.py
@@ -118,3 +118,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
