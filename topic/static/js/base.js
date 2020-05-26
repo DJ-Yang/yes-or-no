63d85487579c506928098fbc69f3e87b9159f44d
parksanghyup <mon2210@naver.com>
2020-05-26 18:10:13 +0900

    footer position fix up

--- topic/static/js/base.js
+++ topic/static/js/base.js
@@ -35,3 +35,31 @@ $('.login').on('click', function(e) {
       $('.popmenu-wrap').addClass('hide');
    }
 });
+
+
+$(document).ready(function() {
+
+   let container_height = $('.container').height();
+   let header_height = $('header').height();   
+   let screen_height = $(window).height();
+
+   
+
+   let check = (container_height+header_height+100)-screen_height;
+
+   // console.log(container_height, header_height, screen_height);
+   // console.log(check);
+
+   if (check<0) {
+      $('footer').css('display','block')
+      $('footer').css('position','absolute');
+      $('footer').css('bottom','0')
+   } else {
+      $('footer').css('display','block')
+      $('footer').css('position','relative');
+   }
+
+   console.log(container_height, header_height, screen_height)
+   
+
+});
