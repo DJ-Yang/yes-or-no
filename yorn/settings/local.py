4b4884e0cbc86d79e1ed4344fcc0886f60600591
DJ-Yang <dong3831@hanmail.net>
2020-05-30 11:58:21 +0900

    devide file path

--- yorn/settings/local.py
+++ yorn/settings/local.py
@@ -20,3 +20,13 @@ CRONTAB_DJANGO_SETTINGS_MODULE = 'yorn.settings.local'
 CRONJOBS = [
     ('* * * * *', 'topic.views.set_hot_topic', '>>'+ os.path.join(BASE_DIR, 'data.log'),)
 ]
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
