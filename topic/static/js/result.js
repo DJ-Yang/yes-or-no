6ff8c1501dcf1c1177df255fc54d93fd9bde6d27
parksanghyup <mon2210@naver.com>
2020-06-01 17:22:32 +0900

    chart position change

--- topic/static/js/result.js
+++ topic/static/js/result.js
@@ -8,13 +8,9 @@ $(document).on('click','.chart1 ul li', function(e) {
 
     reset_canvas1(target.parent().parent().parent());
 
-    if (target.hasClass('week')) {        
-        
-    } else if (target.hasClass('month')) {
-        
-    } else if (target.hasClass('year')) {
-        
-    }
+
+    date = target.attr('class').split(' ')[0]
+    chart_date('date')
     
 });
 
@@ -27,13 +23,16 @@ $(document).on('click','.chart2 ul li', function(e) {
     $('.chart2 ul li').removeClass('active')
     target.addClass('active')
 
-    reset_canvas2(target.parent().parent().parent());
+    
 
     if (target.hasClass('all')) {        
+        reset_canvas2(target.parent().parent().parent(),'all');
         chart_all();  
     } else if (target.hasClass('age')) {
+        reset_canvas2(target.parent().parent().parent(),'age');
         chart_age();
     } else if (target.hasClass('gender')) {
+        reset_canvas2(target.parent().parent().parent(), 'gender');
         chart_gender();
     }
     
@@ -45,10 +44,15 @@ $(document).ready(function() {
 });
 
 
-function reset_canvas2(chart) {
+function reset_canvas2(chart, flag) {
     let canvas = chart.children('.canvas-container2')
     canvas.empty();
-    canvas.append(' <canvas width="100%" height="100%" id="chart2" class="chart2"></canvas>');
+
+    if (flag == 'gender') {
+        canvas.append('<canvas style="width: 180px; height: 180px; margin: 0 auto;" id="chart2-male" class="chart"></canvas><canvas style="width: 180px; height: 180px; margin: 0 auto;" id="chart2-female" class="chart"></canvas>');
+    } else if (flag == 'age') {
+        canvas.append('<canvas width="100%" height="100%" id="chart2" class="chart2"></canvas>');
+    }
 }
 
 function reset_canvas1(chart) {
