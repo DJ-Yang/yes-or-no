eb676c4e7c6a3659674c73a6dc9da7b2cb0e8264
parksanghyup <mon2210@naver.com>
2020-06-04 16:05:18 +0900

    fix all chart error

--- topic/static/js/result.js
+++ topic/static/js/result.js
@@ -44,6 +44,7 @@ $(document).ready(function() {
 });
 
 
+let all_chat_html = $('.canvas-container2').html();
 function reset_canvas2(chart, flag) {
     let canvas = chart.children('.canvas-container2')
     canvas.empty();
@@ -53,7 +54,7 @@ function reset_canvas2(chart, flag) {
     } else if (flag == 'age') {
         canvas.append('<canvas width="100%" hepoweight="100%" id="chart2" class="chart2"></canvas>');
     } else if (flag == 'all') {
-        text = ' <div class="all-chart">                    <div class="chart-title" style="display: flex;">                        <div class="female">여자</div>                        <div class="male">남자</div>                    </div>                    <div class="chart-row">                        <div class="data" data="female-10">                            <div class="agree"></div>                        </div>                        <div class="legend">10대</div>                        <div class="data" data="male-10">                            <div class="agree"></div>                        </div>                    </div>                    <div class="chart-row">                        <div class="data" data="female-10">                            <div class="agree"></div>                        </div>                        <div class="legend">20대</div>                        <div class="data" data="male-10">                            <div class="agree"></div>                        </div>                    </div>                    <div class="chart-row">                        <div class="data" data="female-10">                            <div class="agree"></div>                        </div>                        <div class="legend">30대</div>                        <div class="data" data="male-10">                            <div class="agree"></div>                        </div>                    </div>                    <div class="chart-row">                        <div class="data" data="female-10">                            <div class="agree"></div>                        </div>                        <div class="legend">40대</div>                        <div class="data" data="male-10">                            <div class="agree"></div>                        </div>                    </div>                    <div class="chart-row">                        <div class="data" data="female-10">                            <div class="agree"></div>                        </div>                        <div class="legend">50대</div>                        <div class="data" data="male-10">                            <div class="agree"></div>                        </div>                    </div>      <div class="chart-row"><div class="data" data="female-60"><div class="agree"></div></div><div class="legend">60대</div><div class="data" data="male-60"><div class="agree"></div></div></div>          </div>'
+        text = all_chat_html;
         canvas.append(text);
     }
 }
@@ -127,14 +128,32 @@ $(document).on('mouseenter','.chart-row .data div', function(e) {
         color.css('background-color','#666')
         $('.all-chart-tooltip .value').text($(e.target).parent().attr('data').split('-')[3]+'%')
     }
-
-
 });
 
+// 모바일 환경 툴팁
+$(document).on("touchstart",'.chart-row .data div', function(e){
+    let tooltip = $('.all-chart-tooltip')
+    let color = $('.all-chart-tooltip .square')
+    tooltip.css('top',e.pageY);
+    tooltip.css('left',e.pageX);
+    tooltip.show();
 
+
+    if ($(e.target).hasClass('agree')) {
+        color.css('background-color','#FFDE50')
+        $('.all-chart-tooltip .value').text($(e.target).parent().attr('data').split('-')[2]+'%')
+    } else if ($(e.target).hasClass('disagree')) {
+        color.css('background-color','#666')
+        $('.all-chart-tooltip .value').text($(e.target).parent().attr('data').split('-')[3]+'%')
+    }   
+ });
+
+ 
 $(document).on('mouseleave','.chart-row .data', function(e) {
     $('.all-chart-tooltip').hide();
 });
+
+
 
 
 // 전체차트 value 지정
@@ -161,3 +180,4 @@ function chart_all() {
         $(v).children('.disagree').css('width',disagree);
     });
 }
+
