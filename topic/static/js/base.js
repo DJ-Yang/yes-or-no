7e03c82aaf94dddef2dee3db729ab44f2a6828e9
parksanghyup <mon2210@naver.com>
2020-06-08 22:19:53 +0900

    footer responsive

--- topic/static/js/base.js
+++ topic/static/js/base.js
@@ -39,24 +39,44 @@ $('.login').on('click', function(e) {
 
 $(document).ready(function() {
 
+   check_footer_postion();
+
+   
+});
+
+$(window).resize(function (){
+   check_footer_postion();
+
+});
+
+
+
+function check_footer_postion() {
    let container_height = $('.container').height();
    let header_height = $('header').height();   
    let screen_height = $(window).height();
+   let footer_height = 110;
 
    
 
-   let check = (container_height+header_height+300)-screen_height;
+   let check = (container_height+header_height)-screen_height;
 
-   // console.log(container_height, header_height, screen_height);
+   // console.log(container_height, header_height, screen_height, footer_height);
    // console.log(check);
-
+   // console.log(check+footer_height)
    if (check<0) {
-      $('footer').css('display','block')
-      $('footer').css('position','absolute');
-      $('footer').css('bottom','0')
+      if (check+footer_height>0) {
+         $('footer').css('display','block')
+         $('footer').css('position','relative');
+      } else {
+         $('footer').css('display','block')
+         $('footer').css('position','absolute');
+         $('footer').css('bottom','0')
+      }
+      
    } else {
       $('footer').css('display','block')
       $('footer').css('position','relative');
    }
 
-});
+}
