70ec6c3b79187b54146c3b78acc397bbfd3dd332
DJ-Yang <dong3831@hanmail.net>
2020-06-08 21:47:24 +0900

    fix share button

--- yorn/settings/base.py
+++ yorn/settings/base.py
@@ -149,32 +149,32 @@ CRONJOBS = [
     ('59 23 * * *', 'topic.views.create_daily_data', '>>'+ os.path.join(BASE_DIR, 'data.log'),),
 ]
 
-# 임시 스태틱 루트 설정
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# # 임시 스태틱 루트 설정
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# AWS_LOCATION = 'static'
-# AWS_REGION = get_secret("AWS_REGION")
-# AWS_STORAGE_BUCKET_NAME = get_secret("AWS_STORAGE_BUCKET_NAME")
-# AWS_ACCESS_KEY_ID = get_secret("AWS_ACCESS_KEY_ID")
-# AWS_SECRET_ACCESS_KEY = get_secret("AWS_SECRET_ACCESS_KEY")
-# AWS_QUERYSTRING_AUTH = False
-# AWS_S3_HOST = 's3.%s.amazonaws.com' % AWS_REGION
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_LOCATION = 'static'
+AWS_REGION = get_secret("AWS_REGION")
+AWS_STORAGE_BUCKET_NAME = get_secret("AWS_STORAGE_BUCKET_NAME")
+AWS_ACCESS_KEY_ID = get_secret("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = get_secret("AWS_SECRET_ACCESS_KEY")
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_HOST = 's3.%s.amazonaws.com' % AWS_REGION
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-# # Static Setting
-# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-# STATICFILES_LOCATION = 'static'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# Static Setting
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# # Media Setting
-# MEDIA_URL = "https://%s/media/" % AWS_S3_CUSTOM_DOMAIN
-# MEDIAFILES_LOCATION = 'media'
-# DEFAULT_FILE_STORAGE = 'yorn.settings.storage_backends.MediaStorage'
+# Media Setting
+MEDIA_URL = "https://%s/media/" % AWS_S3_CUSTOM_DOMAIN
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'yorn.settings.storage_backends.MediaStorage'
 
-# # Root Setting
-# STATIC_ROOT = '%s/static' % STORAGE_PATH
-# MEDIA_ROOT = '%s/media' % STORAGE_PATH
+# Root Setting
+STATIC_ROOT = '%s/static' % STORAGE_PATH
+MEDIA_ROOT = '%s/media' % STORAGE_PATH
