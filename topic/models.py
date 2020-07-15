2ebd270a5be821f2f31abb8c0142f9a3f62818b1
DJ-Yang <dong3831@hanmail.net>
2020-07-15 18:47:15 +0900

    Recode (#112)
    * design navbar
    
    * design list page
    
    * list page carousel
    
    * side menu open animation
    
    * model change for expend selection
    
    * add function for combine create topic with selection
    
    * remove daily data for renewal
    
    * select page layout
    
    * fix result page layout
    
    * result page total chart change
    
    * reset views
    
    * delete model method
    
    * renewal topic_select function with form
    
    * change calculator function for today chart
    
    * make daily pick model for period chart
    
    * add hot topic set function
    
    Co-authored-by: parksanghyup <mon2210@naver.com>

--- topic/models.py
+++ topic/models.py
@@ -5,103 +5,52 @@ from user.models import User
 # Integer 필드 최댓값 최솟값 설정을 위한 임포트
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
 class Topic(models.Model):
-  title = models.CharField(max_length=45)
   thumb_image = models.ImageField()
-  description = models.TextField()
-  # 임시로 CharField적용 나중에 Forignkey 로 변경
-  author = models.CharField(max_length= 100, default="admin")
-  selection1_image = models.ImageField()
-  selection1_des = models.CharField(max_length=20)
-  selection2_image = models.ImageField()
-  selection2_des = models.CharField(max_length=20)
+  title = models.CharField(max_length=45)
+  author = models.ForeignKey(User, related_name="topics", on_delete=models.CASCADE)
+  content = models.TextField()
+  # 후에 게시판을 나누게 될 경우 고려
+  # category = models.IntegerField()
+  selection_amount = models.IntegerField(default=0)
   hot_topic = models.BooleanField(default=False)
   created_at = models.DateTimeField(auto_now=timezone.now())
 
-  
-  def get_count_yes(self):
-    total = self.selection_set.count()
-    yes = self.selection_set.filter(select=0).count()
-
-    if total != 0:
-      percent = int(yes/total*100)
-    else:
-      percent = 0
-    return f'{percent}%'
-
-  def get_count_no(self):
-    total = self.selection_set.count()
-    no = self.selection_set.filter(select=1).count()
-    
-
-    if total != 0:
-      percent = int(no/total*100)
-    else:
-      percent = 0
-    return f'{percent}%'
-
-
 class Selection(models.Model):
+  topic = models.ForeignKey(Topic, related_name="selections", on_delete=models.CASCADE)
+  image = models.ImageField()
+  description = models.CharField(max_length=20)
+  created_at = models.DateTimeField(auto_now=timezone.now())
+
+  def save(self, *args, **kwargs):
+    topic = self.topic
+    topic.selection_amount += 1
+    topic.save()
+    super().save(*args, **kwargs)
+
+    # delete 시 삭제 항목
+
+class Pick(models.Model):
   class Sex(models.IntegerChoices):
     MALE = 0
     FEMALE = 1
-  class Select(models.IntegerChoices):
-    YES = 0
-    NO = 1
-  topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-  select = models.IntegerField(choices=Select.choices, null=True, blank=True)
-  selector = models.ForeignKey(User, on_delete=models.CASCADE)
+  topic = models.ForeignKey(Topic, related_name="picks", on_delete=models.CASCADE)
+  author = models.ForeignKey(User, related_name="picks", on_delete=models.CASCADE)
+  selection = models.IntegerField(validators=[
+    MinValueValidator(1), 
+    MaxValueValidator(4),
+  ])
   age_range = models.IntegerField(validators=[
     MinValueValidator(0), 
     MaxValueValidator(100),
-    ])
+  ])
   gender = models.IntegerField(choices=Sex.choices)
   created_at = models.DateTimeField(auto_now=timezone.now())
   updated_at = models.DateTimeField(default=timezone.now)
-
-class DailyData(models.Model):
-  topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
-  positive_count = models.IntegerField(null=True)
-  negative_count = models.IntegerField(null=True)
-  positive_male_count = models.IntegerField(null=True)
-  positive_female_count = models.IntegerField(null=True)
-  negative_male_count = models.IntegerField(null=True)
-  negative_female_count = models.IntegerField(null=True)
-  positive_10age_count = models.IntegerField(null=True)
-  positive_10age_male_count = models.IntegerField(null=True)
-  positive_10age_female_count = models.IntegerField(null=True)
-  positive_20age_count = models.IntegerField(null=True)
-  positive_20age_male_count = models.IntegerField(null=True)
-  positive_20age_female_count = models.IntegerField(null=True)
-  positive_30age_count = models.IntegerField(null=True)
-  positive_30age_male_count = models.IntegerField(null=True)
-  positive_30age_female_count = models.IntegerField(null=True)
-  positive_40age_count = models.IntegerField(null=True)
-  positive_40age_male_count = models.IntegerField(null=True)
-  positive_40age_female_count = models.IntegerField(null=True)
-  positive_50age_count = models.IntegerField(null=True)
-  positive_50age_male_count = models.IntegerField(null=True)
-  positive_50age_female_count = models.IntegerField(null=True)
-  positive_60age_count = models.IntegerField(null=True)
-  positive_60age_male_count = models.IntegerField(null=True)
-  positive_60age_female_count = models.IntegerField(null=True)
-  negative_10age_count = models.IntegerField(null=True)
-  negative_10age_male_count = models.IntegerField(null=True)
-  negative_10age_female_count = models.IntegerField(null=True)
-  negative_20age_count = models.IntegerField(null=True)
-  negative_20age_male_count = models.IntegerField(null=True)
-  negative_20age_female_count = models.IntegerField(null=True)
-  negative_30age_count = models.IntegerField(null=True)
-  negative_30age_male_count = models.IntegerField(null=True)
-  negative_30age_female_count = models.IntegerField(null=True)
-  negative_40age_count = models.IntegerField(null=True)
-  negative_40age_male_count = models.IntegerField(null=True)
-  negative_40age_female_count = models.IntegerField(null=True)
-  negative_50age_count = models.IntegerField(null=True)
-  negative_50age_male_count = models.IntegerField(null=True)
-  negative_50age_female_count = models.IntegerField(null=True)
-  negative_60age_count = models.IntegerField(null=True)
-  negative_60age_male_count = models.IntegerField(null=True)
-  negative_60age_female_count = models.IntegerField(null=True)
-  created_at = models.DateTimeField(default=timezone.now)
+  
+class DailyPick(models.Model):
+  pick1 = models.IntegerField()
+  pick2 = models.IntegerField()
+  pick3 = models.IntegerField()
+  pick4 = models.IntegerField()
+  created_at = models.DateTimeField(auto_now=timezone.now())
