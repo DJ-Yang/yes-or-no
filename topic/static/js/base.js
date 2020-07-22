e3f976e43aa820a9213e535ff4030d54977489ed
parksanghyup <mon2210@naver.com>
2020-07-22 18:46:28 +0900

    Fix conflict merge typing err

--- topic/static/js/base.js
+++ topic/static/js/base.js
@@ -7,13 +7,11 @@ function menuSwitch(e) {
 }
 
 $(document).ready(function() {
-   check_footer_postion();
-   // change_navbar();
+   change_navbar();
 });
 
 $(window).resize(function (){
-   check_footer_postion();
-   // change_navbar();
+   change_navbar();
 });
 
 function change_navbar() {
@@ -26,34 +24,4 @@ function change_navbar() {
       $('.menu-btn').removeClass('hide');
       $('.menu-box').addClass('hide'); 
    }
-}
-
-
-
-function check_footer_postion() {
-   let container_height = $('.container').height();
-   let header_height = $('header').height();   
-   let screen_height = $(window).height();
-   let footer_height = 110;
-
-   
-
-   let check = (container_height+header_height)-screen_height;
-
-
-   if (check<0) {
-      if (check+footer_height>0) {
-         $('footer').css('display','block')
-         $('footer').css('position','relative');
-      } else {
-         $('footer').css('display','block')
-         $('footer').css('position','absolute');
-         $('footer').css('bottom','0')
-      }
-      
-   } else {
-      $('footer').css('display','block')
-      $('footer').css('position','relative');
-   }
-
 }
