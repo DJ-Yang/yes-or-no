3640747764c14f36b978749ff9dc3eb4a0f2852a
parksanghyup <mon2210@naver.com>
2020-07-28 17:57:58 +0900

    Change nav menu, footer position

--- topic/static/js/notice.js
+++ topic/static/js/notice.js
@@ -1,23 +1,24 @@
-$(document).ready(function() {
-    check_footer_postion();
-});
+// $(document).ready(function() {
+//     check_footer_postion();
+// });
 
-function check_footer_postion() {
-    let contents_height = $('.wrapper').height();
-    let screen_height = screen.height;
+// function check_footer_postion() {
+//     let contents_height = $('.wrapper').height();
+//     let screen_height = screen.height;
 
-    console.log(contents_height, screen_height)
+//     console.log(contents_height, screen_height)
 
-    let check = contents_height - screen_height+136;
+//     let check = contents_height - screen_height+136;
  
-    if (check<0) {
-          $('footer').css('display','block')
-          $('footer').css('position','absolute');
-          $('footer').css('bottom','0')       
-    } else {
-       $('footer').css('display','block')
-       $('footer').css('position','relative');
-    }
+//     if (check<0) {
+//         $('body').css('max-height',screen_height);
+//           $('footer').css('display','block')
+//           $('footer').css('position','absolute');
+//           $('footer').css('bottom','0')       
+//     } else {
+//        $('footer').css('display','block')
+//        $('footer').css('position','relative');
+//     }
     
-    $('.test').html(screen_height+' '+contents_height);
- }
+//     $('.test').html(screen_height+' '+contents_height);
+//  }
